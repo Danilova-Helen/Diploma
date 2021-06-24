c2985f512669269229076b56f5ff4a4cb82fe94a
Danilova-Helen <33089165+Danilova-Helen@users.noreply.github.com>
2021-06-24 23:09:01 +0800

    Delete Diploma_7 directory

--- Diploma_7/src/main/java/ru/isu/diploma/repository/HallRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.isu.diploma.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ru.isu.diploma.model.Hall;
-
-@Repository
-public interface HallRepository extends JpaRepository<Hall, Integer> {
-}
